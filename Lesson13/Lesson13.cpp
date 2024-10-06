3f16c80b3ece06738814045c066188fba3857504
Freed31 <abdulganievartem3103@gmail.com>
2024-10-06 13:57:46 +0200

    Добавьте файлы проекта.

---
+++ Lesson13/Lesson13.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <memory>
+#include <string>
+
+class Toy {
+public:
+    Toy(const std::string& name) : name_(name) {}
+
+    std::string getName() const {
+        return name_;
+    }
+
+    ~Toy() {
+        std::cout << "Toy " << name_ << " was dropped." << std::endl;
+    }
+
+private:
+    std::string name_;
+};
+
+class Dog {
+public:
+    Dog(const std::string& name) : name_(name) {}
+
+    void getToy(std::shared_ptr<Toy> toy) {
+        if (toy_ == toy) {
+            std::cout << "I already have this toy." << std::endl;
+        }
+        else if (toy.use_count() > 1) {
+            std::cout << "Another dog is playing with this toy." << std::endl;
+        }
+        else {
+            toy_ = toy;
+            std::cout << name_ << " picked up the toy " << toy->getName() << "." << std::endl;
+        }
+    }
+
+    void dropToy() {
+        if (toy_) {
+            std::cout << name_ << " dropped the toy " << toy_->getName() << "." << std::endl;
+            toy_.reset();
+        }
+        else {
+            std::cout << "Nothing to drop." << std::endl;
+        }
+    }
+
+private:
+    std::string name_;
+    std::shared_ptr<Toy> toy_;
+};
+
+int main() {
+    auto toy = std::make_shared<Toy>("Bone");
+    Dog dog1("Rex");
+    Dog dog2("Max");
+
+    dog1.getToy(toy);
+    dog2.getToy(toy);
+
+    dog1.dropToy();
+    dog2.getToy(toy);
+
+    return 0;
+}
---
+++ Lesson13_Ex2/Lesson13_Ex2.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <utility>
+#include <string>
+
+class Toy {
+public:
+    Toy(const std::string& name) : name_(name) {}
+
+    std::string getName() const {
+        return name_;
+    }
+
+    ~Toy() {
+        std::cout << "Toy " << name_ << " was dropped." << std::endl;
+    }
+
+private:
+    std::string name_;
+};
+
+class shared_ptr_toy {
+public:
+    shared_ptr_toy() : ptr_(nullptr), count_(nullptr) {}
+
+    explicit shared_ptr_toy(Toy* toy) : ptr_(toy), count_(new size_t(1)) {}
+
+    shared_ptr_toy(const shared_ptr_toy& other) : ptr_(other.ptr_), count_(other.count_) {
+        if (count_) {
+            ++(*count_);
+        }
+    }
+
+    shared_ptr_toy(shared_ptr_toy&& other) noexcept : ptr_(std::exchange(other.ptr_, nullptr)),
+        count_(std::exchange(other.count_, nullptr)) {}
+
+    shared_ptr_toy& operator=(const shared_ptr_toy& other) {
+        if (this != &other) {
+            release();
+            ptr_ = other.ptr_;
+            count_ = other.count_;
+            if (count_) {
+                ++(*count_);
+            }
+        }
+        return *this;
+    }
+
+    shared_ptr_toy& operator=(shared_ptr_toy&& other) noexcept {
+        if (this != &other) {
+            release();
+            ptr_ = std::exchange(other.ptr_, nullptr);
+            count_ = std::exchange(other.count_, nullptr);
+        }
+        return *this;
+    }
+
+    ~shared_ptr_toy() {
+        release();
+    }
+
+    Toy& operator*() const {
+        return *ptr_;
+    }
+
+    Toy* operator->() const {
+        return ptr_;
+    }
+
+    size_t use_count() const {
+        return count_ ? *count_ : 0;
+    }
+
+private:
+    Toy* ptr_;
+    size_t* count_;
+
+    void release() {
+        if (count_ && --(*count_) == 0) {
+            delete ptr_;
+            delete count_;
+        }
+    }
+};
+
+shared_ptr_toy make_shared_toy(const std::string& name) {
+    return shared_ptr_toy(new Toy(name));
+}
+
+shared_ptr_toy make_shared_toy(const shared_ptr_toy& other) {
+    return shared_ptr_toy(new Toy(*other));
+}
+
+int main() {
+    auto toy1 = make_shared_toy("Ball");
+    std::cout << "Toy created: " << toy1->getName() << ", Use count: " << toy1.use_count() << std::endl;
+
+    {
+        auto toy2 = toy1;
+        std::cout << "Toy copied: " << toy2->getName() << ", Use count: " << toy1.use_count() << std::endl;
+    }
+
+    std::cout << "After toy2 is out of scope, Use count: " << toy1.use_count() << std::endl;
+
+    return 0;
+}
